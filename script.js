f10322e76ea9659031cab59f5162bd8357e8631f
Juanky <juancamiloherrera960@gmail.com>
2023-08-20 21:23:48 -0500

    Cambios visuales y mejoras de verificacion

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ function validarForm (e) {
     const edad = document.querySelector("#age").value
 
     const validacion = document.querySelector("#validacion");
-    validacion.textContent = `Hola ${nombre}! tienes ${edad} años y eres ${verificarEdad()}`
+    validacion.textContent = `Hola ${nombre}, tienes ${edad} años y ${verificarEdad()}.`
 }
 
 function verificarEdad () {
     const edad = document.querySelector("#age").value;
 
-    if (edad >= 18 && edad < 60) {
-        return "mayor de edad";
-        
-    } else if (edad >= 60) {
-        return "adulto mayor";
+    if (edad > 0 && edad < 17) {
+        return "eres menor de edad";
+
+    } else if (edad >= 18 && edad < 60) {
+        return "eres mayor de edad";
+
+    } else if (edad >= 60 && edad <= 115) {
+        return "eres adulto mayor";
     }
 
     else {
-        return "menor de edad";
+        return "tienes una edad incorrecta"
     }
 }
