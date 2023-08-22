c8f92aaf6315344c41c9d91ffa7202f1076a370c
Juanky <juancamiloherrera960@gmail.com>
2023-08-22 15:45:31 -0500

    Documentacion, animaciones, alerta mobile
    Se agrega documentación, nuevas animaciones, alertas si se está presentando la página en un celular, y mejoras para evitar datos incorrectos en los inputs

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const formulario = document.querySelector("#form")
 
-formulario.addEventListener("submit", validarForm)
+// Escucha cuando carga la página y ejecuta la función "classImportant" a los 1100ms
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(classImportant, 1100);   
+});
 
-function validarForm (e) {
-    e.preventDefault();
-    const nombre = document.querySelector("#name").value
-    const edad = document.querySelector("#age").value
+// Función que añade clase que contiene transition con !important a los inputs y el btn.
+function classImportant () {
+    const btn = document.querySelector(".btnInside");
+    const form = document.querySelectorAll(".inputForm")
 
-    const validacion = document.querySelector("#validacion");
-    validacion.textContent = `Hola ${nombre}, tienes ${edad} años y ${verificarEdad()}.`
+    btn.classList.add("btnInsideNew");
+    form[0].classList.add("inputFormNew");
+    form[1].classList.add("inputFormNew");
 }
 
+
+// Escucha si el usuario le da click en el btn tipo submit y ejecuta la función "ValidarForm".
+const formulario = document.querySelector("#form")
+formulario.addEventListener("submit", validarForm)
+
+
+// Función que valida la edad del usuario y dependiendo retorna un texto u otro.
 function verificarEdad () {
     const edad = document.querySelector("#age").value;
 
@@ -28,3 +38,39 @@ function verificarEdad () {
         return "tienes una edad incorrecta"
     }
 }
+
+// Función que obtiene el nombre y la edad del usuario y crea el texto.
+function validarForm (e) {
+    e.preventDefault();
+    const nombre = document.querySelector("#name").value
+    const edad = document.querySelector("#age").value
+
+    const validacion = document.querySelector("#validacion");
+
+    // Verificar que los campos no esten vacíos y posteriormente generar el texto
+    if (nombre && edad != "") {
+        validacion.textContent = `Hola ${nombre}, tienes ${edad} años y ${verificarEdad()}.`
+    } else {
+        validacion.textContent = "Por favor introduce los datos correspondientes."
+    }
+
+    formulario.reset();
+}
+
+// Función para verificar si se está abriendo la página desde un dispositivo móvil
+function isMobile () {
+    return window.innerWidth <= 526;
+}
+
+// Si se está abriendo en un celular muestra una alerta
+function handleMobile() {
+    if (isMobile()) {
+        alert("Disculpe este sitio web no está optimizado para dispositivos móviles :(")
+        
+        // Eliminar evento que escucha si redimensionan la página para evitar múltiples alertas
+        window.removeEventListener("resize", handleMobile);
+    }
+}
+
+window.addEventListener("resize", handleMobile);
+document.addEventListener("DOMContentLoaded", handleMobile);
